ec660d05702a9f6902b2f60be7d59e753c1ab5d0
Gustavo Omai <omai.gustavoh@gmail.com>
2026-08-24 15:14:16 -0300

    Fix | Related products

--- assets/aomai-crosssell.js
+++ assets/aomai-crosssell.js
@@ -4,21 +4,6 @@ if (!customElements.get('aomai-crosssell')) {
     class extends HTMLElement {
       connectedCallback() {
         this.addEventListener('change', this.onVariantChange);
-        if (!this.querySelector('.aomai-crosssell__card')) this.load();
-      }
-
-      async load() {
-        try {
-          const response = await fetch(
-            `${this.dataset.url}&product_id=${this.dataset.productId}&section_id=${this.dataset.sectionId}`
-          );
-          const doc = new DOMParser().parseFromString(response.ok ? await response.text() : '', 'text/html');
-
-          if (doc.querySelector('.aomai-crosssell__card')) this.innerHTML = doc.querySelector('aomai-crosssell').innerHTML;
-          else this.remove();
-        } catch {
-          this.remove();
-        }
       }
 
       onVariantChange({ target }) {
